28f329eaa5317b00dbf6c94941ef069b7388ca28
Chelsea Rios <chelsea.rios@igt.com>
2022-06-10 13:10:25 -0700

    Installer added Renamed program Cleaned up code Added the star on paperwork

---
+++ Generate_Installer.ps1
@@ -0,0 +1,6 @@
+$ProjectDirectory = "C:\Users\Ohana\PycharmProjects\SPCA-Automation"
+New-Item -ItemType Directory -Force -Path $ProjectDirectory\inst\
+Set-Location -Path $ProjectDirectory
+venv\Scripts\pyinstaller.exe .\VaccineClinicPaperwork\ClinicPaperwork.py -y
+
+Start-Process -FilePath "C:\Program Files (x86)\NSIS\makensis.exe" -Wait -Verb "runAs" -WindowStyle "hidden" -ArgumentList "$($ProjectDirectory)\installer.nsi"
